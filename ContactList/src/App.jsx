59bbfaa72ac638f41d3c1b6aae39001fdc7c6bf1
jeremy-jefferson <jeremyjefferson34@gmail.com>
2023-08-07 08:23:05 -0500

    update + add components

--- ContactList/src/App.jsx
+++ ContactList/src/App.jsx
@@ -1,28 +1,21 @@
-import React, { useState } from 'react'; // Keep the import of 'useState' since it is used.
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact.jsx";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-      <div>
-      
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit 
-          <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        
-      </p>
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
-
-export default App;
